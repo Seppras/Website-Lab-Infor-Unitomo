bbd8906a818473e17409f4524cff50dde8043fc3
Sepras <shirokisaragi32@gmail.com>
2024-10-18 00:29:04 +0700

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,72 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ["Inter", "sans-serif"],
+      },
+      textColor: {
+        custom1: "#000581",
+        custom2: "#146C94",
+      },
+      letterSpacing: {
+        jauh: "0.525rem",
+      },
+      lineHeight: {
+        jarak1: "5.0rem",
+      },
+      backgroundColor: {
+        custom1: "#76E1FF",
+        custom2: "#B6D0FD",
+        custom3: "#DFF6FF",
+      },
+      rotate: {
+        10: "360deg",
+      },
+      margin: {
+        "18px": "18px",
+        "17px": "17px",
+        "85px": "84.4px",
+        "107px": "107px",
+        "108px": "108px",
+        "109px": "109px",
+        "110px": "110px",
+        "114px": "114px",
+        "115px": "115px",
+        "116px": "116px",
+        "118px": "118px",
+        "119px": "119px",
+        "120px": "120px",
+        "122px": "122px",
+        "350px": "388px",
+      },
+      width: {
+        "165px": "165px",
+        "120px": "120px",
+        "72px": "72px",
+        "100px": "100px",
+        "137px": "137px",
+        "516px": "516px",
+        "850px": "952px",
+        "273px": "230px",
+        "471px": "471px",
+      },
+      height: {
+        "165px": "165px",
+        "120px": "120px",
+        "72px": "72px",
+        "100px": "100px",
+        "113px": "113px",
+        "516px": "516px",
+        "625px": "625px",
+        "225px": "200px",
+        "533px": "533px",
+      },
+      borderRadius: {
+        lingk: "512px",
+      },
+    },
+  },
+  plugins: [],
+};
